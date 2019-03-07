b8ce74a8bc51aba2a86b2a7b479f22c99e83cedd
庄培龙 <peilong.zhuang@office.feng.com>
2019-03-07 13:34:52 +0800

    update

--- AllSetting/mysql_setting.py
+++ AllSetting/mysql_setting.py
@@ -1,34 +1,54 @@
 import aiomysql
 import asyncio
 
+
 class GetMysqlSetting:
     def __init__(self):
         pass
 
-    async def insert_mysql(self,loop,data):
-        pool = await aiomysql.create_pool(host='192.168.9.227',port=3306,user='root',password='123456',db='google_play',charset='utf8',autocommit=True,loop=loop)
+
+    async def insert_mysql(self,loop, data):
+        pool = await aiomysql.create_pool(host='192.168.9.227', port=3306, user='root', password='123456',
+                                         db='google_play', charset='utf8', autocommit=True, loop=loop)
         async with pool.get() as conn:
             async with conn.cursor() as cur:
                 if data["country"] == "us":
                     to_mysql = "crawl_google_play_app_info"
                 else:
                     to_mysql = "crawl_google_play_app_info_" + data["country"]
-                sql = 'insert into ' + to_mysql + '(id,language,appsize,category,contentrating,current_version,description,developer,whatsnew,developer_url,instalations,isbusy,last_updatedate,minimum_os_version,name,pkgname,url) VALUES(default,"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'.format(data["country"], data["size"], data["category"], data["content_rating"],
-                                           data["app_version"],
-                                           data["description"], data["provider"], data["what_news"],
-                                           data["developer_url"], data["installs"],
-                                           data["is_busy"], data["update_time"], data["min_os_version"],
-                                           data["name"], data["pkgname"], data["url"])
+                sql = 'insert into ' + to_mysql + '(id,language,appsize,category,contentrating,current_version,description,developer,whatsnew,developer_url,instalations,isbusy,last_updatedate,minimum_os_version,name,pkgname,url) VALUES(default,"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'.format(
+                    data["country"], data["size"], data["category"], data["content_rating"],
+                    data["app_version"],
+                    data["description"], data["provider"], data["what_news"],
+                    data["developer_url"], data["installs"],
+                    data["is_busy"], data["update_time"], data["min_os_version"],
+                    data["name"], data["pkgname"], data["url"])
                 try:
                     await cur.execute(sql)
+                    print('存入成功')
                 except Exception as e:
-                    print("数据库语句:"+sql)
+                    print("数据库语句:" + sql)
                     print('数据库错误信息：' + str(e))
 
 
-    def run(self,loop,data):
+    def run(self, loop,data):
         task = asyncio.ensure_future(self.insert_mysql(loop,data))
         return task
+
+
 if __name__ == '__main__':
+
+    # 测试
+    loop = asyncio.get_event_loop()
+    data = {'pkgname': 'test', 'url': 'test', 'country': 'us', 'update_time': '2019-02-28 00:00:00', 'size': '36M',
+            'installs': '1,000,000+', 'app_version': '36.7.6', 'min_os_version': '4.0 and up',
+            'content_rating': 'Everyone', 'provider': 'Dialekts', 'developer_email': 'dialekts.game@gmail.com',
+            'is_busy': 0, 'name': 'Shoot Dinosaur Eggs',
+            'developer_url': 'https://play.google.com/store/apps/developer?id=Dialekts', 'category': 'Casual',
+            'app_current_num': '4,478',
+            'cover_image_url': 'https://lh3.googleusercontent.com/OHew4iL5WhbpvZ-oAvr8-wPQ-_N77SvaAOfCzkGeUruYPuDx1gTScEmrXy1nlcwvGEk=s180-rw',
+            'description': 'Configure the combination and shoot down the balls of the same color. The game has many different levels, with each new level to knock the egg becomes more difficult.',
+            'what_news': '✔ Added adaptive icon.,✔ We have improved performance.,✔ Fixed bugs.,Thanks for playing. Be sure to rate us after each update.'}
+
     t = GetMysqlSetting()
-    t.run()
+    loop.run_until_complete(t.run(loop,data))
--- CheckOut_ApkName/check_update_apkname.py
+++ CheckOut_ApkName/check_update_apkname.py
@@ -221,41 +221,41 @@ class CheckUpdateApkname:
 
                 if len(check_tasks) >= 1:
                     check_results = self.loop.run_until_complete(asyncio.gather(*check_tasks))
+                    redis_tasks = []
+                    print('存入数据库')
+                    save_mysql_tasks = []
+                    check_other_tasks = []
+                    for check_result in check_results:
+                        try:
+                            data_return, analysis_data = check_result
+                            if data_return != None:
+                                task = asyncio.ensure_future(self.save_redis(data_return))
+                                redis_tasks.append(task)
+                            if analysis_data != None:
+                                print('添加数据库任务')
+                                task = asyncio.ensure_future(self.send_mysql.run(self.loop,analysis_data))
+                                save_mysql_tasks.append(task)
+                            if data_return != None and data_return["is_update"] == 1:
+                                task = asyncio.ensure_future(self.check_other_coutry(data_return))
+                                check_other_tasks.append(task)
+                        except Exception as e:
+                            print('错误信息：' + str(e))
+                            print(check_results)
+                    if len(redis_tasks) >= 1:
+                        self.loop.run_until_complete(asyncio.wait(redis_tasks))
 
-                redis_tasks = []
-                print('存入数据库')
-                save_mysql_tasks = []
-                check_other_tasks = []
-                for check_result in check_results:
-                    try:
-                        data_return, analysis_data = check_result
-                        if data_return != None:
-                            task = asyncio.ensure_future(self.save_redis(data_return))
-                            redis_tasks.append(task)
-                        if analysis_data != None:
-                            task = asyncio.ensure_future(self.send_mysql.run(self.loop,analysis_data))
-                            save_mysql_tasks.append(task)
-                        if data_return != None and data_return["is_update"] == 1:
-                            task = asyncio.ensure_future(self.check_other_coutry(data_return))
-                            check_other_tasks.append(task)
-                    except Exception as e:
-                        print('错误信息：' + str(e))
-                        print(check_results)
-                if len(redis_tasks) >= 1:
-                    self.loop.run_until_complete(asyncio.wait(redis_tasks))
+                    if len(check_other_tasks) >= 1:
+                        check_other_results = self.loop.run_until_complete(asyncio.gather(*check_other_tasks))
+                        for result in check_other_results:
+                            print("结果:" + str(result))
+                            if result != None:
+                                print("国家:" + result["country"])
+                                task = self.send_mysql.run(self.loop,result)
+                                save_mysql_tasks.append(task)
 
-                if len(check_other_tasks) >= 1:
-                    check_other_results = self.loop.run_until_complete(asyncio.gather(*check_other_tasks))
-                    for result in check_other_results:
-                        print("结果:" + str(result))
-                        if result != None:
-                            print("国家:" + result["country"])
-                            task = self.send_mysql.run(self.loop, result)
-                            save_mysql_tasks.append(task)
-
-
-                if len(save_mysql_tasks) >= 1:
-                    self.loop.run_until_complete(asyncio.wait(save_mysql_tasks))
+                    if len(save_mysql_tasks) >= 1:
+                        print('执行存入数据库')
+                        self.loop.run_until_complete(asyncio.wait(save_mysql_tasks))
 
 
 if __name__ == '__main__':
