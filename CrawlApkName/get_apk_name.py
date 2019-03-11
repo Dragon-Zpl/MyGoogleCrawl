786168a8148adaa767002ec90272195ee64053a7
庄培龙 <peilong.zhuang@office.feng.com>
2019-03-11 16:08:04 +0800

    update

--- CrawlApkName/get_apk_name.py
+++ CrawlApkName/get_apk_name.py
@@ -69,27 +69,30 @@ class CrawlApkName:
 
     async def fetch_post_apkname(self,url,data):
         proxy = await self.get_proxy()
-        # try:
-        data = await self._Request.post_request(self.session,url,proxy,data)
-        analysis_data = etree.HTML(data)
-        apknames = analysis_data.xpath(
-            "//div[@class='card no-rationale square-cover apps small']//span[@class='preview-overlay-container']/@data-docid")
-        for apkname in apknames:
-            self.apk_names.add(apkname)
-        # except Exception as e:
-        #     self.printf.info(str(e))
-        #     try:
-        #         self.proxies.remove(proxy)
-        #     except:
-        #         pass
+        try:
+            data = await self._Request.post_request(self.session,url,proxy,data)
+            analysis_data = etree.HTML(data)
+            apknames = analysis_data.xpath(
+                "//div[@class='card no-rationale square-cover apps small']//span[@class='preview-overlay-container']/@data-docid")
+            for apkname in apknames:
+                self.apk_names.add(apkname)
+        except Exception as e:
+            self.printf.info(str(e))
+            try:
+                self.proxies.remove(proxy)
+            except:
+                pass
 
     async def fetch_get_apkname(self,url):
         proxy = await self.get_proxy()
         try:
+            print(str(url)+'代理为：'+str(proxy))
             data = await self._Request.get_request(self.session,url,proxy)
+            print('获得数据')
             analysis_data = etree.HTML(data)
             apknames = analysis_data.xpath(
                 "//div[@class='card no-rationale square-cover apps small']//span[@class='preview-overlay-container']/@data-docid")
+            print('apknames'+str(apknames))
             for apkname in apknames:
                 self.apk_names.add(apkname)
         except Exception as e:
